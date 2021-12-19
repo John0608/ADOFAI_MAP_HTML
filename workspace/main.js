28aca7820dfceee47a08a4774c1e7c19c172020c
John0608 <72543106+John0608@users.noreply.github.com>
2021-12-19 23:48:11 +0900

    Add files via upload

---
+++ workspace/main.js
@@ -0,0 +1,38 @@
+var upload_Result;
+var obj;
+
+const fileUpload = () => {
+    String.prototype.replaceAll = function(org, dest) {
+        return this.split(org).join(dest);
+    }
+    
+    const parseLevel = level => {
+        try {
+            return JSON5.parse(String(level).trim());
+        } catch (e) {
+            return JSON5.parse(String(level).trim()
+                .replaceAll(', ,', ',')
+                .replaceAll('}\n', '},\n')
+                .replaceAll('},\n\t]', '}\n\t]')
+                .replaceAll(', },', ' },')
+                .replaceAll(', }', ' }')
+                .replaceAll('\n', '')
+                .replaceAll('}\n', '},\n'));
+        }
+    }
+    
+    const input = document.querySelector('#file_select_btn')
+    const f = input.files[0]
+    if (!f) return
+    const reader = new FileReader()
+    reader.onload = (evt) => {
+        const level = parseLevel(evt.target.result)
+        console.log(level)
+        upload_Result = level; 
+    }
+    reader.onerror = () => {
+      return alert('error reading file')
+    }
+    reader.readAsText(f, 'UTF-8')
+  }
+
