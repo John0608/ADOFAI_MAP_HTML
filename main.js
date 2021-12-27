ed6e9d976b16b6d9eab20f18863174beefc5240b
John0608 <72543106+John0608@users.noreply.github.com>
2021-12-27 23:53:43 +0900

    Update main.js
    이벤트 이름과 해당 이벤트의 고유한 키값으로 지원하지 않는 키 제거 기능 구현

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const fileUpload = () => {
   function fix()
   {
       Level.actions = Level.actions.filter(x=>effect_List.includes(x.eventType))
-      Remove_MoveCamera_Keys();
+      Remove_Keys("MoveCamera", MoveCamera_List);
 
       let bpm = Level.settings.bpm;
       for(var i = 0; i < Level.actions.length; i++)
@@ -79,18 +79,18 @@ const fileUpload = () => {
     document.body.removeChild(element)
 }
 
-function Remove_MoveCamera_Keys() //카메라 중 지원하지 않는 키 제거
+function Remove_Keys(event_name, event_keys) //카메라 중 지원하지 않는 키 제거
 {
     var actions_length = Level.actions.length;  //actions 길이
     for(var i = 0; i < actions_length; i++)     //actions 길이만큼 반복
     {
-        if(Level.actions[i].eventType == "MoveCamera")
+        if(Level.actions[i].eventType == event_name)
         {
             var key = Object.getOwnPropertyNames(Level.actions[i]);
             console.log("Key :" + key.toString());
             for(var o = 0; o < key.length; o++)
             {
-                if(MoveCamera_List.indexOf(key[o]) == -1)
+                if(event_keys.indexOf(key[o]) == -1)
                 {
                     var result = delete Level.actions[i][key[o]];
                     console.log(result);
