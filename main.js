d383df55ab3e7d6c495adb3e37c0f7e74a614c82
John0608 <72543106+John0608@users.noreply.github.com>
2021-12-29 23:40:18 +0900

    스크립트 수정 및 Ui 추가
    1. 로그? 따위를 만듬.
    2. ui 대충 만듬
    3. 스크립트 수정
    4. 이펙트 수정 추가

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 var Level;
 var obj;
-
-var effect_List = ["SetSpeed","Twirl","CustomBackground","ChangeTrack","ColorTrack","AnimateTrack","AddDecoration","Flash","MoveCamera","SetHitsound","RecolorTrack","MoveTrack","HallOfMirrors","ShakeScreen","SetPlanetRotation","MoveDecorations","RepeatEvents"];
-var MoveCamera_List = ["floor", "eventType", "duration", "relativeTo", "position", "rotation", "zoom", "angleOffset", "ease"];
+var FileName = "main.adofai";
 var effectList = {
-    "CustomBackground" : "sd"
+    "CustomBackground": ['floor', 'eventType', 'color', 'bgImage', 'imageColor', 'parallax', 'bgDisplayMode', 'lockRot', 'loopBG', 'unscaledSize', 'angleOffset'],
+    //"ChangeTrack" : [],
+    "ColorTrack": ['floor', 'eventType', 'trackColorType', 'trackColor', 'secondaryTrackColor', 'trackColorAnimDuration', 'trackColorPulse', 'trackPulseLength', 'trackStyle'],
+    "AnimateTrack": ['floor', 'eventType', 'trackAnimation', 'beatsAhead', 'trackDisappearAnimation', 'beatsBehind'],
+    "AddDecoration": ['floor', 'eventType', 'decorationImage', 'position', 'relativeTo', 'pivotOffset', 'rotation', 'scale', 'depth', 'tag'],
+    "Flash": ['floor', 'eventType', 'duration', 'plane', 'startColor', 'startOpacity', 'endColor', 'endOpacity', 'angleOffset'],
+    "MoveCamera": ["floor", "eventType", "duration", "relativeTo", "position", "rotation", "zoom", "angleOffset", "ease"],
+    //"SetHitsound" : [],
+    //"RecolorTrack" : [],
+    "MoveTrack": ['floor', 'eventType', 'startTile', 'endTile', 'duration', 'positionOffset', 'rotationOffset', 'scale', 'opacity', 'angleOffset', 'ease'],
+    "HallOfMirrors": ['floor', 'eventType', 'enabled', 'angleOffset'],
+    "ShakeScreen": ['floor', 'eventType', 'duration', 'strength', 'intensity', 'fadeOut', 'angleOffset'],
+    "SetPlanetRotation": ['floor', 'eventType', 'ease'],
+    "MoveDecorations": ['floor', 'eventType', 'duration', 'tag', 'positionOffset', 'rotationOffset', 'scale', 'angleOffset', 'ease']
+    //"RepeatEvents" : []
+
 };
+var ul_list = $(".ul_list");
+
 const fileUpload = () => {
-    String.prototype.replaceAll = function(org, dest) {
+    String.prototype.replaceAll = function (org, dest) {
         return this.split(org).join(dest);
     }
-    
+
     const parseLevel = level => {
         try {
             return JSON5.parse(String(level).trim());
@@ -25,50 +40,36 @@ const fileUpload = () => {
                 .replaceAll('}\n', '},\n'));
         }
     }
-    
+
     const input = document.querySelector('#file_select_btn')
     const f = input.files[0]
     if (!f) return
     const reader = new FileReader()
     reader.onload = (evt) => {
         const level = parseLevel(evt.target.result)
-        Level = level; 
-        //fix();
-        download("main.adofai",JSON.stringify(Level));
+        Level = level;
+        $(".console").show();
+        fix();
+
     }
     reader.onerror = () => {
-      return alert('error reading file')
+        return alert('error reading file')
     }
     reader.readAsText(f, 'UTF-8')
-  }
+}
 
-  function fix()
-  {
-      Level.actions = Level.actions.filter(x=>effect_List.includes(x.eventType)) //이펙트 필터링
-      fix_BPM();
-  }
+function fix() {
+    Level.actions = Level.actions.filter(x => Object.keys(effectList).includes(x.eventType)) //이펙트 필터링
+    fix_BPM();
+    fix_actions();
+    document.querySelector('.download').style.display = 'block';
+}
 
-  function fix_BPM()
-  {
-    let bpm = Level.settings.bpm;
-    Level.actions.forEach(function(index){            
-        if(index.eventType == "SetSpeed")
-        {
-            if(index.speedType == "Multiplier")
-            {
-                index.speedType = "Bpm";
-                bpm = bpm * index.bpmMultiplier;
-                index.bpmMultiplier = 1;
-                index.beatsPerMinute = bpm;
-            }
-            else {
-                bpm = index.beatsPerMinute;
-            }
-        }
-    });
-  }
+function down_click() {
+    download("main.adofai", JSON.stringify(Level));
+}
 
-  function download(filename, text) {
+function download(filename, text) {
     const element = document.createElement('a')
     element.setAttribute(
         'href',
@@ -84,50 +85,80 @@ const fileUpload = () => {
     document.body.removeChild(element)
 }
 
-function Remove_Keys(event_name, event_keys)
-{
-    var actions_length = Level.actions.length;  //actions 길이
-    for(var i = 0; i < actions_length; i++)     //actions 길이만큼 반복
-    {
-        if(Level.actions[i].eventType == event_name)
-        {
-            var key = Object.getOwnPropertyNames(Level.actions[i]);
-            console.log("Key :" + key.toString());
-            for(var o = 0; o < key.length; o++)
-            {
-                if(event_keys.indexOf(key[o]) == -1)
-                {
-                    var result = delete Level.actions[i][key[o]];
-                    console.log(result);
-                    
-                }
+function fix_BPM() {
+    let bpm = Level.settings.bpm;
+    Level.actions.forEach(function (index) {
+        if (index.eventType == "SetSpeed") {
+            if (index.speedType == "Multiplier") {
+                index.speedType = "Bpm";
+                bpm = bpm * index.bpmMultiplier;
+                index.bpmMultiplier = 1;
+                index.beatsPerMinute = bpm;
+            }
+            else {
+                bpm = index.beatsPerMinute;
             }
         }
-        else {}
-    }
+    });
+    addText("BPM 수정 완료")
+
 }
 
-effect_List.forEach(function(element, index){
-    console.log(element, index)
-});
-
-function Remove_Keys2(event_name, event_keys)
+function addText(str)
 {
-    Level.actions.forEach(function(index){
-        if(index.eventType == event_name)
-        {
+    
+    var ul_list = $(".ul_list");
+    ul_list.append("<li>"+str+"</li>");
+}
+
+
+function Remove_Keys(event_name, event_keys) {
+    Level.actions.forEach(function (index) {
+        if (index.eventType == event_name) {
             var key = Object.getOwnPropertyNames(index);
             console.log("Key :" + key.toString());
-            for(var o = 0; o < key.length; o++)
-            {
-                if(event_keys.indexOf(key[o]) == -1)
-                {
+            for (var o = 0; o < key.length; o++) {
+                if (event_keys.indexOf(key[o]) == -1) {
                     var result = delete index[key[o]];
                     console.log(result);
-                    
+
                 }
             }
         }
-        else {}
+        else { }
+        
     })
 }
+
+function fix_actions() {
+    Object.keys(effectList).forEach(function (index) {
+         const evtName = index;
+         const evtArray = effectList[index];
+         Remove_Keys(evtName, evtArray);
+         addText(index + " 수정 완료")
+        })
+
+}
+
+
+
+/*      먼저 만든 키 제거기
+function Remove_Keys(event_name, event_keys) {
+    var actions_length = Level.actions.length;  //actions 길이
+    for (var i = 0; i < actions_length; i++)     //actions 길이만큼 반복
+    {
+        if (Level.actions[i].eventType == event_name) {
+            var key = Object.getOwnPropertyNames(Level.actions[i]);
+            console.log("Key :" + key.toString());
+            for (var o = 0; o < key.length; o++) {
+                if (event_keys.indexOf(key[o]) == -1) {
+                    var result = delete Level.actions[i][key[o]];
+                    console.log(result);
+
+                }
+            }
+        }
+        else { }
+    }
+}
+*/
