c579d14c5aa62218a7b6539de4604191f6ba2e03
John0608 <72543106+John0608@users.noreply.github.com>
2022-01-29 15:54:15 +0900

    Add Edit to Basic Setting
    맵 기본설정으로 바꾸는 기능을 추가하였습니다. 또 맵 이펙트가 지워지지 않는 오류를 해결하였습니다. 기본설정 및 이펙트 리스트를 adofai.json에 지속적 추가할 예정입니다.

--- main.js
+++ main.js
@@ -1,40 +1,247 @@
+// "AnimateTrack ChangeTrack CustomBackground SetHitsound RecolorTrack" 도움받기
 var Level;
 var obj;
 var FileName = "main.adofai";
 var effect = {
-    "List":
-    {
-        "SetSpeed": ["floor", "eventType", "speedType", "beatsPerMinute", "bpmMultiplier"],
-        "CustomBackground": ['floor', 'eventType', 'color', 'bgImage', 'imageColor', 'parallax', 'bgDisplayMode', 'lockRot', 'loopBG', 'unscaledSize', 'angleOffset'],
-        "ChangeTrack" : [],
-        "ColorTrack": ['floor', 'eventType', 'trackColorType', 'trackColor', 'secondaryTrackColor', 'trackColorAnimDuration', 'trackColorPulse', 'trackPulseLength', 'trackStyle'],
-        "AnimateTrack": ['floor', 'eventType', 'trackAnimation', 'beatsAhead', 'trackDisappearAnimation', 'beatsBehind'],
-        "AddDecoration": ['floor', 'eventType', 'decorationImage', 'position', 'relativeTo', 'pivotOffset', 'rotation', 'scale', 'depth', 'tag'],
-        "Flash": ['floor', 'eventType', 'duration', 'plane', 'startColor', 'startOpacity', 'endColor', 'endOpacity', 'angleOffset'],
-        "MoveCamera": ["floor", "eventType", "duration", "relativeTo", "position", "rotation", "zoom", "angleOffset", "ease"],
-        "SetHitsound" : [],
-        "RecolorTrack" : [],
-        "MoveTrack": ['floor', 'eventType', 'startTile', 'endTile', 'duration', 'positionOffset', 'rotationOffset', 'scale', 'opacity', 'angleOffset', 'ease'],
-        "HallOfMirrors": ['floor', 'eventType', 'enabled', 'angleOffset'],
-        "ShakeScreen": ['floor', 'eventType', 'duration', 'strength', 'intensity', 'fadeOut', 'angleOffset'],
-        "SetPlanetRotation": ['floor', 'eventType', 'ease'],
-        "MoveDecorations": ['floor', 'eventType', 'duration', 'tag', 'positionOffset', 'rotationOffset', 'scale', 'angleOffset', 'ease']
-       /// //"RepeatEvents" : []
-    },
     "Setting": {
-        "AddDecoration": { "relativeTo": ['Global', 'Tile'] },
-        "ColorTrack" : {
-            'trackColorType' : ['Single','Stripes','Glow','Blink','Switch','Rainbow'],
-            'trackColorPulse' : ['Forward', 'Backward'],
-            'trackStyle' : ['Standard','Neon','NeonLight','Gems']
-        },
-        "Flash" : {
-            'plane' : ['Foreground','Background']
+        "Key": [
+            "version",
+            "artist",
+            "song",
+            "author",
+            "hitsound",
+            "hitsoundVolume",
+            "separateCountdownTime",
+            "songFilename",
+            "bpm",
+            "volume",
+            "offset",
+            "pitch",
+            "trackColorType",
+            "trackColor",
+            "secondaryTrackColor",
+            "trackColorAnimDuration",
+            "trackColorPulse",
+            "trackPulseLength",
+            "trackStyle",
+            "trackAnimation",
+            "beatsAhead",
+            "trackDisappearAnimation",
+            "beatsBehind",
+            "backgroundColor",
+            "bgImage",
+            "bgImageColor",
+            "parallax",
+            "bgDisplayMode",
+            "lockRot",
+            "loopBG",
+            "unscaledSize",
+            "relativeTo",
+            "position",
+            "rotation",
+            "zoom",
+            "bgVideo",
+            "loopVideo",
+            "vidOffset",
+            "floorIconOutlines",
+            "stickToFloors",
+            "planetEase"
+        ],
+        "basic_value": {
+            "hitsound": "Kick",
+            "trackColorType": "Single",
+            "trackColor": "debb7b",
+            "secondaryTrackColor": "ffffff",
+            "trackColorAnimDuration": 2,
+            "trackColorPulse": "None",
+            "trackPulseLength": 10,
+            "trackStyle": "Standard",
+            "trackAnimation": "None",
+            "beatsAhead": 3,
+            "trackDisappearAnimation": "None",
+            "beatsBehind": 4,
+            "backgroundColor": "000000",
+            "bgImage": "",
+            "bgImageColor": "ffffff",
+            "parallax": [
+                100,
+                100
+            ],
+            "bgDisplayMode": "FitToScreen",
+            "lockRot": "Disabled",
+            "loopBG": "Disabled",
+            "unscaledSize": 100,
+            "relativeTo": "Player",
+            "position": [
+                0,
+                0
+            ],
+            "rotation": 0,
+            "zoom": 100,
+            "bgVideo": "",
+            "loopVideo": "Disabled",
+            "vidOffset": 0,
+            "floorIconOutlines": "Disabled",
+            "stickToFloors": "Disabled",
+            "planetEase": "Linear"
         }
-        // "AnimateTrack ChangeTrack CustomBackground SetHitsound RecolorTrack" 도움받기
-
+    },
+    "effect": {
+        "List": {
+            "SetSpeed": [
+                "floor",
+                "eventType",
+                "speedType",
+                "beatsPerMinute",
+                "bpmMultiplier"
+            ],
+            "CustomBackground": [
+                "floor",
+                "eventType",
+                "color",
+                "bgImage",
+                "imageColor",
+                "parallax",
+                "bgDisplayMode",
+                "lockRot",
+                "loopBG",
+                "unscaledSize",
+                "angleOffset"
+            ],
+            "ColorTrack": [
+                "floor",
+                "eventType",
+                "trackColorType",
+                "trackColor",
+                "secondaryTrackColor",
+                "trackColorAnimDuration",
+                "trackColorPulse",
+                "trackPulseLength",
+                "trackStyle"
+            ],
+            "AnimateTrack": [
+                "floor",
+                "eventType",
+                "trackAnimation",
+                "beatsAhead",
+                "trackDisappearAnimation",
+                "beatsBehind"
+            ],
+            "AddDecoration": [
+                "floor",
+                "eventType",
+                "decorationImage",
+                "position",
+                "relativeTo",
+                "pivotOffset",
+                "rotation",
+                "scale",
+                "depth",
+                "tag"
+            ],
+            "Flash": [
+                "floor",
+                "eventType",
+                "duration",
+                "plane",
+                "startColor",
+                "startOpacity",
+                "endColor",
+                "endOpacity",
+                "angleOffset"
+            ],
+            "MoveCamera": [
+                "floor",
+                "eventType",
+                "duration",
+                "relativeTo",
+                "position",
+                "rotation",
+                "zoom",
+                "angleOffset",
+                "ease"
+            ],
+            "MoveTrack": [
+                "floor",
+                "eventType",
+                "startTile",
+                "endTile",
+                "duration",
+                "positionOffset",
+                "rotationOffset",
+                "scale",
+                "opacity",
+                "angleOffset",
+                "ease"
+            ],
+            "HallOfMirrors": [
+                "floor",
+                "eventType",
+                "enabled",
+                "angleOffset"
+            ],
+            "ShakeScreen": [
+                "floor",
+                "eventType",
+                "duration",
+                "strength",
+                "intensity",
+                "fadeOut",
+                "angleOffset"
+            ],
+            "SetPlanetRotation": [
+                "floor",
+                "eventType",
+                "ease"
+            ],
+            "MoveDecorations": [
+                "floor",
+                "eventType",
+                "duration",
+                "tag",
+                "positionOffset",
+                "rotationOffset",
+                "scale",
+                "angleOffset",
+                "ease"
+            ]
+        },
+        "Settings_key": {
+            "AddDecoration": {
+                "relativeTo": [
+                    "Global",
+                    "Tile"
+                ]
+            },
+            "ColorTrack": {
+                "trackColorType": [
+                    "Single",
+                    "Stripes",
+                    "Glow",
+                    "Blink",
+                    "Switch",
+                    "Rainbow"
+                ],
+                "trackColorPulse": [
+                    "Forward",
+                    "Backward"
+                ],
+                "trackStyle": [
+                    "Standard",
+                    "Neon",
+                    "NeonLight",
+                    "Gems"
+                ]
+            },
+            "Flash": {
+                "plane": [
+                    "Foreground",
+                    "Background"
+                ]
+            }
+        }
     }
-};
+}
 var setting_List = ["version", "artist", "specialArtistType", "artistPermission", "song", "author", "separateCountdownTime", "previewImage", "previewIcon", "previewIconColor", "previewSongStart", "previewSongDuration", "seizureWarning", "levelDesc", "levelTags", "artistLinks", "difficulty", "songFilename", "bpm", "volume", "offset", "pitch", "hitsound", "hitsoundVolume", "countdownTicks", "trackColorType", "trackColor", "secondaryTrackColor", "trackColorAnimDuration", "trackColorPulse", "trackPulseLength", "trackStyle", "trackAnimation", "beatsAhead", "trackDisappearAnimation", "beatsBehind", "backgroundColor", "bgImage", "bgImageColor", "parallax", "bgDisplayMode", "lockRot", "loopBG", "unscaledSize", "relativeTo", "position", "rotation", "zoom", "bgVideo", "loopVideo", "vidOffset", "floorIconOutlines", "stickToFloors", "planetEase", "planetEaseParts"];
 var ul_list = $(".ul_list");
 
@@ -66,7 +273,7 @@ const fileUpload = () => {
         const level = parseLevel(evt.target.result)
         Level = level;
         $(".console").show();
-        fix();
+        //fix();
 
     }
     reader.onerror = () => {
@@ -77,7 +284,11 @@ const fileUpload = () => {
 
 function fix() {
     Level.actions = Level.actions.filter(x => Object.keys(effect.List).includes(x.eventType)) //이펙트 필터링
-
+            
+    if($("#map_basic").is(":checked") == true)
+    {
+        fix_setting_basic();
+    }
     fix_actions();
     fix_BPM();
     document.querySelector('.download').style.display = 'block';
@@ -148,9 +359,9 @@ function Remove_Keys(event_name, event_keys) {
 }
 
 function fix_actions() {
-    Object.keys(effect.List).forEach(function (index) {
+    Object.keys(effect.effect.List).forEach(function (index) {
         const evtName = index;
-        const evtArray = effect.List[index];
+        const evtArray = effect.effect.List[index];
         Remove_Keys(evtName, evtArray);
         addText(index + " 수정 완료")
     });
@@ -167,8 +378,22 @@ function fix_actions() {
 
 }
 
+function fix_settings()
+{
+    Object.keys(Level.settings).forEach(function (index) {
+        if(effect.Setting.Key.indexOf(index) == -1)
+         {
+             delete Level.settings[index];
+         }
+    })
+}
 
-
+function fix_setting_basic()
+{
+    Object.keys(effect.Setting.basic_value).forEach(function (index) {
+        Level.settings[index] = effect.Setting.basic_value[index];
+    })
+}
 
 
 /*      먼저 만든 키 제거기
