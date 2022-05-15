a3ace0c2ff0650b8dd066a308f2fd1863938770a
John0608 <72543106+John0608@users.noreply.github.com>
2022-05-15 23:28:54 +0900

    그냥 스크립트 한가운데 다 두자...ㄹㅇ
    힘들다 ㄹㅇ 발전도 없어 이게 뭥 ㅑㅜㅜ

--- Beta/assets/js/main.js
+++ Beta/assets/js/main.js
@@ -15,8 +15,9 @@ function ReadFile()
     let Zfile = null;
     function rf(_callback)
     {
-        let zf = upload.upload_FileRead();
+        let zf = upload_FileRead();
         Zfile = zf;
+        zipFile = zf;
         _callback();
     }
     function log() {
@@ -68,3 +69,50 @@ function addSelect(list)
     })
 
 }
+
+function upload_FileRead() {
+    const input = document.querySelector('.file_select_btn')
+    const f = input.files[0]
+    const fName = f.name.substring(f.name.lastIndexOf("."));
+    if (!f) return
+    else if (fName != ".zip") {
+        alert("Zip 파일만 지원합니다.")
+        this.upload_init();
+        return false;
+    }
+    else {
+        const reader = new FileReader()
+        reader.onload = function (evt) {
+            JSZip.loadAsync(evt.target.result).then(
+                function (zip) {
+                    zipFile = zip;
+                }
+                ).then(() => {
+                    if (upload.isUpload(zipFile) == true) {
+                        let level = adofai_class.findLevel(zipFile);
+                        if (level.length >= 0 && level != false) {
+                        addSelect(level);
+                        alert("읽음");
+                        ui.Show(".select");
+                        ui.Hide("#info_msg");
+                        console.log("읽기 완료");
+                        console.log(zipFile)
+                        return zipFile;
+                    }
+                    else {
+                        alert("레벨 파일을 찾을 수 없습니다.");
+                        return;
+                    }
+                }
+                return zipFile;
+            })
+        }
+        reader.onerror = () => {
+            alert('파일을 읽는데 실패하였습니다.')
+            return false;
+        }
+        reader.readAsArrayBuffer(f, 'UTF-8');
+    }
+
+
+};
